b78d430113a8bb6c10c1c41c4b076d50fca919ac
Mitch Bigelow <ipha00@gmail.com>
2013-11-10 15:54:10 -0500

    update

--- scrabble-gui.py
+++ scrabble-gui.py
@@ -6,76 +6,78 @@ import scrabble
 import pickle
 
 class ScrabbleGUI(QtGui.QMainWindow):
-	def __init__(self):
+	def __init__(self, parent=None):
+		QtGui.QMainWindow.__init__(self, parent)
 		# Load layout
-		self.ui = uic.loadUi("scrabble-gui.ui")
+		uic.loadUi("scrabble-gui.ui", self)
+
+		# Create solver instance
+		self.solver = scrabble.Solver(scrabble.WORDSWITHFRIENDS)
 
 		# Create tile backgrounds
 		for x in range(0, 15):
 			for y in range(0, 15):
-				self.ui.board.setItem(y, x, QtGui.QTableWidgetItem(' '))
-				self.ui.board.item(y, x).setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-				self.ui.board.item(y, x).setFont(QtGui.QFont("Sans", 10, QtGui.QFont.Bold))
+				self.board.setItem(y, x, QtGui.QTableWidgetItem(' '))
+				self.board.item(y, x).setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+				self.board.item(y, x).setFont(QtGui.QFont("Sans", 10, QtGui.QFont.Bold))
 				# Double letter
-				if scrabble.letter_mult[y][x] == 2:
-					self.ui.board.item(y, x).setBackground(QtGui.QColor(0, 0, 127, 100))
+				if self.solver.letter_mult[y][x] == 2:
+					self.board.item(y, x).setBackground(QtGui.QColor(0, 0, 127, 100))
 				# Triple letter
-				elif scrabble.letter_mult[y][x] == 3:
-					self.ui.board.item(y, x).setBackground(QtGui.QColor(0, 170, 0, 100))
+				elif self.solver.letter_mult[y][x] == 3:
+					self.board.item(y, x).setBackground(QtGui.QColor(0, 170, 0, 100))
 				# Double word
-				elif scrabble.word_mult[y][x] == 2:
-					self.ui.board.item(y, x).setBackground(QtGui.QColor(170, 0, 0, 100))
+				elif self.solver.word_mult[y][x] == 2:
+					self.board.item(y, x).setBackground(QtGui.QColor(170, 0, 0, 100))
 				# Triple word
-				elif scrabble.word_mult[y][x] == 3:
-					self.ui.board.item(y, x).setBackground(QtGui.QColor(255, 170, 0, 100))
+				elif self.solver.word_mult[y][x] == 3:
+					self.board.item(y, x).setBackground(QtGui.QColor(255, 170, 0, 100))
 		# Center
-		self.ui.board.item(7, 7).setBackground(QtGui.QColor(0, 0, 0, 100))
+		self.board.item(7, 7).setBackground(QtGui.QColor(0, 0, 0, 100))
 
 
 		# Event handlers
-		self.connect(self.ui.solve, QtCore.SIGNAL("clicked()"), self.solve)
-		self.connect(self.ui.save, QtCore.SIGNAL("clicked()"), self.save)
-		self.connect(self.ui.load, QtCore.SIGNAL("clicked()"), self.load)
+		self.connect(self.solve_btn, QtCore.SIGNAL("clicked()"), self.solve)
+		self.connect(self.save_btn, QtCore.SIGNAL("clicked()"), self.save)
+		self.connect(self.load_btn, QtCore.SIGNAL("clicked()"), self.load)
 
 		self.load()
 
 		# Show window
-		self.ui.show()
+		self.show()
 	def solve(self):
-		scrabble.tiles = self.ui.letters.text()
-
 		for x in range(0, 15):
 			for y in range(0, 15):
-				char = self.ui.board.item(y, x).text()
+				char = self.board.item(y, x).text()
 				if char in "abcdefghijklmnopqrstuvwxyz":
-					scrabble.board[y][x] = char
+					self.solver.list_board[y][x] = char
 				else:
-					self.ui.board.item(y, x).setText(' ')
-					scrabble.board[y][x] = ' '
+					self.board.item(y, x).setText(' ')
+					self.solver.list_board[y][x] = ' '
 
-		valid_words = scrabble.solve()
+		valid_words = self.solver.solve(self.letters.text())
 
-		self.ui.results.clear()
-		for word in sorted(scrabble.unique(valid_words), key=lambda word: word[4]):
+		self.results.clear()
+		for word in valid_words:
 			if word[3] == scrabble.HORIZONTAL:
-				self.ui.results.append("Horz: x: %2i  y: %2i score: %i word: %s" % (word[1]+1, word[2]+1, word[4], word[0]))
+				self.results.append("Horz: x: %2i  y: %2i score: %i word: %s" % (word[1]+1, word[2]+1, word[4], word[0]))
 			if word[3] == scrabble.VERTICAL:
-				self.ui.results.append("Vert: x: %2i  y: %2i score: %i word: %s" % (word[1]+1, word[2]+1, word[4], word[0]))
+				self.results.append("Vert: x: %2i  y: %2i score: %i word: %s" % (word[1]+1, word[2]+1, word[4], word[0]))
 
 	def save(self):
 		data = {
-			"board": scrabble.board,
+			"board": self.solver.list_board,
 			"tiles": ""
 		}
 		for x in range(0, 15):
 			for y in range(0, 15):
-				char = self.ui.board.item(y, x).text()
+				char = self.board.item(y, x).text()
 				if char in "abcdefghijklmnopqrstuvwxyz":
 					data["board"][y][x] =  char
 				else:
-					self.ui.board.item(y, x).setText(' ')
+					self.board.item(y, x).setText(' ')
 					data["board"][y][x] = ' '
-		data["tiles"] = self.ui.letters.text()
+		data["tiles"] = self.letters.text()
 
 		# TODO choose save file
 		save_file = open('data.pkl', 'wb')
@@ -89,10 +91,10 @@ class ScrabbleGUI(QtGui.QMainWindow):
 			data = pickle.load(save_file)
 			save_file.close()
 
-			self.ui.letters.setText(data["tiles"])
+			self.letters.setText(data["tiles"])
 			for x in range(0, 15):
 				for y in range(0, 15):
-					self.ui.board.item(y, x).setText(data["board"][y][x])
+					self.board.item(y, x).setText(data["board"][y][x])
 		except:
 			print("Couldn't load file")
 
@@ -100,6 +102,7 @@ class ScrabbleGUI(QtGui.QMainWindow):
 def main():
 	app = QtGui.QApplication(sys.argv)
 	window = ScrabbleGUI()
+	window.show()
 	sys.exit(app.exec_())
 
 if __name__ == '__main__':
